e307935ac3683328119b12d300f8a162ded201e3
sira72 <120383642+sira72@users.noreply.github.com>
2023-01-03 19:57:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 66. Найти сумму нат.элементов от М до N.
+
+// M=1; N=10 ->120
+// M=4; N=9 ->30
+
+int SumNaturalNums(int n, int m)
+{
+    if (n>m) return 0;
+    else return n+SumNaturalNums(n+1,m);
+}
